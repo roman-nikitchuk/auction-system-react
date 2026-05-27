4355db8df6125486af13bc144af5306bc58db370
Artem3383 <makodar2157_aehit@students.vizja.pl>
2026-05-27 20:50:27 +0200

    Merge pull request #2 from roman-nikitchuk/api-connection

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,12 @@
 import axios from "axios";
-
 const api = axios.create({
   baseURL: "https://localhost:7219/api",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
-
 export default api;
