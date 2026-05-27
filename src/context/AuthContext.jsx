d565e7724f3c085d868aaeba6791c01b46c687d2
Artem3383 <makodar2157_aehit@students.vizja.pl>
2026-05-27 20:54:11 +0200

    feat: axios instance and auth context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState } from 'react';
 import api from '../api/axios';
 const AuthContext = createContext();
+
 export function AuthProvider({ children }) {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [user, setUser] = useState(
